3ffea999e34bfad435e7d04048379428659e1258
Naveed Ashfaq <naveedashfaq914@gmail.com>
2020-01-20 21:20:11 -0500

    Completed intersection function for planes

--- computer-graphics-ray-casting/src/Plane.cpp
+++ computer-graphics-ray-casting/src/Plane.cpp
@@ -4,9 +4,15 @@
 bool Plane::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
+  double denom = this->normal.dot(ray.direction);
+  if (denom == 0)
+    return false;
+  double numer = this->normal.dot(this->point) - this->normal.dot(ray.origin);
+  t = numer / denom;
+  if (t >= min_t) {
+    n = this->normal;
+    return true;
+  }
   return false;
-  ////////////////////////////////////////////////////////////////////////////
 }
 
