ebb8d5d3b4e6e934b97ece661060c68ca7ad2c87
Naveed Ashfaq <naveedashfaq914@gmail.com>
2020-01-22 19:38:17 -0500

    Fixed issue with viewing_ray.cpp by multiplying camera transform matrix with ray.direction

--- computer-graphics-ray-casting/src/viewing_ray.cpp
+++ computer-graphics-ray-casting/src/viewing_ray.cpp
@@ -12,5 +12,11 @@ void viewing_ray(
   ray.direction[0] = (camera.width * (j + 0.5) / width) - (camera.width / 2.0);
   ray.direction[1] = (camera.height / 2.0) - (camera.height * (i + 0.5) / height);
   ray.direction[2] = -1 * camera.d;
+
+  Eigen::Matrix3d cam_transform;
+  cam_transform << camera.u, camera.v, camera.w;
+
+  ray.direction = cam_transform * ray.direction;
+
 }
 
