419ada9fd26c34eae8884e03865ef82743bf2f7f
Naveed Ashfaq <naveedashfaq914@gmail.com>
2020-01-29 21:24:46 -0500

    Fixed error where point light max_t was always 1 as direction was normalized too early

--- computer-graphics-ray-tracing/src/PointLight.cpp
+++ computer-graphics-ray-tracing/src/PointLight.cpp
@@ -3,7 +3,7 @@
 void PointLight::direction(
   const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
 {
-  d = (this->p - q).normalized();
+  d = this->p - q;
   max_t = d.norm();
   d = d.normalized();
 }
