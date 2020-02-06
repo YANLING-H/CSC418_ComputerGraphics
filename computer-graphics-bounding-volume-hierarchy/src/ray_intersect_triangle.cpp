b2a31c517f307e8ebfdcf0427cdcd4c9491b3ba4
Naveed Ashfaq <naveedashfaq914@gmail.com>
2020-02-06 15:37:57 -0500

    Implemented brute force triangle mesh intersection

--- computer-graphics-bounding-volume-hierarchy/src/ray_intersect_triangle.cpp
+++ computer-graphics-bounding-volume-hierarchy/src/ray_intersect_triangle.cpp
@@ -1,4 +1,5 @@
 #include "ray_intersect_triangle.h"
+#include <Eigen/Dense>
 
 bool ray_intersect_triangle(
   const Ray & ray,
@@ -9,10 +10,25 @@ bool ray_intersect_triangle(
   const double max_t,
   double & t)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  t = 0;
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+  Eigen::Vector3d c = A.transpose();       // Take first point as join point of triangle
+  // The two arms of the triangle
+  Eigen::Vector3d t1 = B.transpose() - c; 
+  Eigen::Vector3d t2 = C.transpose() - c;
+
+  Eigen::Matrix3d m;
+  m << t1, t2, -1 * ray.direction;
+
+  if (m.determinant() == 0)
+    return false;   // The matrix m is not linearly dependant, so no hit possible
+
+  Eigen::Vector3d values = m.inverse() * (ray.origin - c);    // Values of alpha, beta and t
+
+  if (values[0] < 0 || values[1] < 0 || 
+    values[0] + values[1] > 1 || values[2] < min_t || values[2] > max_t)
+      return false;   // Values of alpha, beta and/or t do not meet constraints
+
+  // Triangle hit is valid
+  t = values[2];
+  return true;
 }
 
--- computer-graphics-bounding-volume-hierarchy/src/ray_intersect_triangle_mesh_brute_force.cpp
+++ computer-graphics-bounding-volume-hierarchy/src/ray_intersect_triangle_mesh_brute_force.cpp
@@ -1,6 +1,8 @@
 #include "ray_intersect_triangle_mesh_brute_force.h"
 #include "ray_intersect_triangle.h"
 
+#include <limits>
+
 bool ray_intersect_triangle_mesh_brute_force(
   const Ray & ray,
   const Eigen::MatrixXd & V,
@@ -10,10 +12,15 @@ bool ray_intersect_triangle_mesh_brute_force(
   double & hit_t,
   int & hit_f)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  hit_t = 0;
-  hit_f = 0;
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+  bool hit_found = false;
+  double closest_hit = std::numeric_limits<double>::infinity();
+  double t = 0.0;
+  for (int i=0; i < F.rows(); ++i) {
+    if (ray_intersect_triangle(ray, V.row(F(i,0)), V.row(F(i,1)), V.row(F(i,2)), min_t, max_t, t) && t < closest_hit) {
+      hit_t = t;
+      hit_f = i;
+      hit_found = true;
+    }
+  }
+  return hit_found;
 }
