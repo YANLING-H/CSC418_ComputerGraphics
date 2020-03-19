2aba48c0259aa58eaef113f311429b63af8774b6
Naveed Ashfaq <naveedashfaq914@gmail.com>
2020-03-19 00:07:13 -0400

    Implemented sparse functions. Completed Lab 8

--- computer-graphics-mass-spring-systems/src/fast_mass_springs_precomputation_sparse.cpp
+++ computer-graphics-mass-spring-systems/src/fast_mass_springs_precomputation_sparse.cpp
@@ -15,14 +15,31 @@ bool fast_mass_springs_precomputation_sparse(
   Eigen::SparseMatrix<double>  & C,
   Eigen::SimplicialLLT<Eigen::SparseMatrix<double> > & prefactorization)
 {
-  /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code
+
+  r.resize(E.rows());
+  for (int i=0; i<E.rows(); ++i)
+    r(i) = (V.row(E(i, 0)) - V.row(E(i, 1))).norm();
+
   std::vector<Eigen::Triplet<double> > ijv;
-  const int n = V.rows();
-  for(int i = 0;i<n;i++) ijv.emplace_back(i,i,1);
-  Eigen::SparseMatrix<double> Q(n,n);
-  Q.setFromTriplets(ijv.begin(),ijv.end());
-  /////////////////////////////////////////////////////////////////////////////
+
+  M.resize(V.rows(), V.rows());
+  for (int i=0; i<V.rows(); ++i)
+    ijv.emplace_back(i, i, m[i]);
+  
+  M.setFromTriplets(ijv.begin(), ijv.end());
+  ijv.clear();
+
+  signed_incidence_matrix_sparse(V.rows(), E, A);
+
+  C.resize(b.rows(), V.rows());
+  for (int i=0; i<b.rows(); ++i)
+    ijv.emplace_back(i, b[i], 1);
+  
+  C.setFromTriplets(ijv.begin(), ijv.end());
+
+  double w = 1e10;
+  Eigen::SparseMatrix<double> Q = (k * A.transpose() * A) + (M / (delta_t * delta_t)) + w * C.transpose() * C;
+
   prefactorization.compute(Q);
   return prefactorization.info() != Eigen::NumericalIssue;
 }
